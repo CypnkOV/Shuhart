959a4f9a2e049b9710b2dc8685df23e92460b986
CypnkOV <major01_91@mail.ru>
2022-10-30 00:58:57 +0300

    0.5.5

--- Shuhart.py
+++ Shuhart.py
@@ -3,7 +3,7 @@ import numpy as np
 import random as rand
 import pandas as pd
 
-mesh1 = [rand.randint(0, 80), rand.randint(0, 10), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80)]
+mesh1 = [rand.randint(0, 50), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100)]
 pepe1 = pd.DataFrame({'x': mesh1})
 razmah = []
 
@@ -22,6 +22,7 @@ pepe2 = pd.DataFrame({'x': razmah})
 n1 = n2 = n3 = n4 = n5 = n6 = n7 = n8 = n9 = -98989
 m = 0
 a1 = 0
+a2 = 0
 X = 0
 R = 0
 UCL1 = 0
@@ -53,7 +54,7 @@ while len(mesh1) != 9:
     L1 = X - l11
     L11 = X - l1
     m += 1
-    if m > UCL1: # Первый критерий
+    if m > UCL1:
         mesh1.insert(2, m)
         razmah.clear()
         razschet()
@@ -76,7 +77,7 @@ while len(mesh1) != 15:
     l11 = 2 / 3 * (X - LCL1)
     L1 = X - l11
     L11 = X - l1
-    a1 += 2
+    a1 += rand.randint(4, 10)
     if a1 > L1:
         mesh1.insert(6, a1)
         razmah.clear()
@@ -84,7 +85,7 @@ while len(mesh1) != 15:
         pepe1 = pd.DataFrame({'x': mesh1})
         pepe2 = pd.DataFrame({'x': razmah})
 
-# дописать третий критерий
+# 3 критерий
 
 while len(mesh1) != 24 and n1 < L1 and n2 < L1 and n3 < L1 and n4 < L1 and n5 < L1 and n6 < L1 and n7 < L1 and n8 < L1 and n9 < L1:
     X = np.nanmean(pepe1.x)
@@ -100,6 +101,30 @@ while len(mesh1) != 24 and n1 < L1 and n2 < L1 and n3 < L1 and n4 < L1 and n5 < 
     l11 = 2 / 3 * (X - LCL1)
     L1 = X - l11
     L11 = X - l1
+    if mesh1[1] > UCL1:
+        mesh1[1] = int(UCL1 - 25)
+    if m < UCL1:
+        X = np.nanmean(pepe1.x)  # средняя для Индивид. (nanmean её вычисляет)
+        R = np.nanmean(pepe2.x)  # средняя для Размахов
+        UCL1 = X + 2.660 * R
+        LCL1 = X - 2.660 * R
+        UCL2 = 3.267 * R
+        u1 = 1 / 3 * (UCL1 - X)
+        u11 = 2 / 3 * (UCL1 - X)
+        U1 = UCL1 - u11
+        U11 = UCL1 - u1
+        l1 = 1 / 3 * (X - LCL1)
+        l11 = 2 / 3 * (X - LCL1)
+        L1 = X - l11
+        L11 = X - l1
+        m = int(UCL1 + 25)
+        if m > UCL1:
+            mesh1.pop(2)
+            mesh1.insert(2, m)
+            razmah.clear()
+            razschet()
+            pepe1 = pd.DataFrame({'x': mesh1})
+            pepe2 = pd.DataFrame({'x': razmah})
     if len(mesh1) != 24:
         mesh1.append(0)
         mesh1.append(0)
@@ -111,7 +136,7 @@ while len(mesh1) != 24 and n1 < L1 and n2 < L1 and n3 < L1 and n4 < L1 and n5 < 
         mesh1.append(0)
         mesh1.append(0)
     while n1 < L11:
-        n1 = rand.randint(int(L1), int(X))
+        n1 = rand.randint(int(L11), int(X))
         mesh1[15] = n1
         razmah.clear()
         razschet()
@@ -131,163 +156,59 @@ while len(mesh1) != 24 and n1 < L1 and n2 < L1 and n3 < L1 and n4 < L1 and n5 < 
         pepe1 = pd.DataFrame({'x': mesh1})
         pepe2 = pd.DataFrame({'x': razmah})
         while n2 < L11:
-            n2 = rand.randint(int(L1), int(X))
+            n2 = rand.randint(int(L11), int(X))
             mesh1[16] = n2
             razmah.clear()
             razschet()
-            X = np.nanmean(pepe1.x)
-            R = np.nanmean(pepe2.x)
-            UCL1 = X + 2.660 * R
-            LCL1 = X - 2.660 * R
-            UCL2 = 3.267 * R
-            u1 = 1 / 3 * (UCL1 - X)
-            u11 = 2 / 3 * (UCL1 - X)
-            U1 = UCL1 - u11
-            U11 = UCL1 - u1
-            l1 = 1 / 3 * (X - LCL1)
-            l11 = 2 / 3 * (X - LCL1)
-            L1 = X - l11
-            L11 = X - l1
             pepe1 = pd.DataFrame({'x': mesh1})
             pepe2 = pd.DataFrame({'x': razmah})
             while n3 < L11:
-                n3 = rand.randint(int(L1), int(X))
+                n3 = rand.randint(int(L11), int(X))
                 mesh1[17] = n3
                 razmah.clear()
                 razschet()
-                X = np.nanmean(pepe1.x)
-                R = np.nanmean(pepe2.x)
-                UCL1 = X + 2.660 * R
-                LCL1 = X - 2.660 * R
-                UCL2 = 3.267 * R
-                u1 = 1 / 3 * (UCL1 - X)
-                u11 = 2 / 3 * (UCL1 - X)
-                U1 = UCL1 - u11
-                U11 = UCL1 - u1
-                l1 = 1 / 3 * (X - LCL1)
-                l11 = 2 / 3 * (X - LCL1)
-                L1 = X - l11
-                L11 = X - l1
                 pepe1 = pd.DataFrame({'x': mesh1})
                 pepe2 = pd.DataFrame({'x': razmah})
                 while n4 < L11:
-                    n4 = rand.randint(int(L1), int(X))
+                    n4 = rand.randint(int(L11), int(X))
                     mesh1[18] = n4
                     razmah.clear()
                     razschet()
-                    X = np.nanmean(pepe1.x)
-                    R = np.nanmean(pepe2.x)
-                    UCL1 = X + 2.660 * R
-                    LCL1 = X - 2.660 * R
-                    UCL2 = 3.267 * R
-                    u1 = 1 / 3 * (UCL1 - X)
-                    u11 = 2 / 3 * (UCL1 - X)
-                    U1 = UCL1 - u11
-                    U11 = UCL1 - u1
-                    l1 = 1 / 3 * (X - LCL1)
-                    l11 = 2 / 3 * (X - LCL1)
-                    L1 = X - l11
-                    L11 = X - l1
                     pepe1 = pd.DataFrame({'x': mesh1})
                     pepe2 = pd.DataFrame({'x': razmah})
                     while n5 < L11:
-                        n5 = rand.randint(int(L1), int(X))
+                        n5 = rand.randint(int(L11), int(X))
                         mesh1[19] = n5
                         razmah.clear()
                         razschet()
-                        X = np.nanmean(pepe1.x)
-                        R = np.nanmean(pepe2.x)
-                        UCL1 = X + 2.660 * R
-                        LCL1 = X - 2.660 * R
-                        UCL2 = 3.267 * R
-                        u1 = 1 / 3 * (UCL1 - X)
-                        u11 = 2 / 3 * (UCL1 - X)
-                        U1 = UCL1 - u11
-                        U11 = UCL1 - u1
-                        l1 = 1 / 3 * (X - LCL1)
-                        l11 = 2 / 3 * (X - LCL1)
-                        L1 = X - l11
-                        L11 = X - l1
                         pepe1 = pd.DataFrame({'x': mesh1})
                         pepe2 = pd.DataFrame({'x': razmah})
                         while n6 < L11:
-                            n6 = rand.randint(int(L1), int(X))
+                            n6 = rand.randint(int(L11), int(X))
                             mesh1[20] = n6
                             razmah.clear()
                             razschet()
-                            X = np.nanmean(pepe1.x)
-                            R = np.nanmean(pepe2.x)
-                            UCL1 = X + 2.660 * R
-                            LCL1 = X - 2.660 * R
-                            UCL2 = 3.267 * R
-                            u1 = 1 / 3 * (UCL1 - X)
-                            u11 = 2 / 3 * (UCL1 - X)
-                            U1 = UCL1 - u11
-                            U11 = UCL1 - u1
-                            l1 = 1 / 3 * (X - LCL1)
-                            l11 = 2 / 3 * (X - LCL1)
-                            L1 = X - l11
-                            L11 = X - l1
                             pepe1 = pd.DataFrame({'x': mesh1})
                             pepe2 = pd.DataFrame({'x': razmah})
                             while n7 < L11:
-                                n7 = rand.randint(int(L1), int(X))
+                                n7 = rand.randint(int(L11), int(X))
                                 mesh1[21] = n7
                                 razmah.clear()
                                 razschet()
-                                X = np.nanmean(pepe1.x)
-                                R = np.nanmean(pepe2.x)
-                                UCL1 = X + 2.660 * R
-                                LCL1 = X - 2.660 * R
-                                UCL2 = 3.267 * R
-                                u1 = 1 / 3 * (UCL1 - X)
-                                u11 = 2 / 3 * (UCL1 - X)
-                                U1 = UCL1 - u11
-                                U11 = UCL1 - u1
-                                l1 = 1 / 3 * (X - LCL1)
-                                l11 = 2 / 3 * (X - LCL1)
-                                L1 = X - l11
-                                L11 = X - l1
                                 pepe1 = pd.DataFrame({'x': mesh1})
                                 pepe2 = pd.DataFrame({'x': razmah})
                                 while n8 < L11:
-                                    n8 = rand.randint(int(L1), int(X))
+                                    n8 = rand.randint(int(L11), int(X))
                                     mesh1[22] = n8
                                     razmah.clear()
                                     razschet()
-                                    X = np.nanmean(pepe1.x)
-                                    R = np.nanmean(pepe2.x)
-                                    UCL1 = X + 2.660 * R
-                                    LCL1 = X - 2.660 * R
-                                    UCL2 = 3.267 * R
-                                    u1 = 1 / 3 * (UCL1 - X)
-                                    u11 = 2 / 3 * (UCL1 - X)
-                                    U1 = UCL1 - u11
-                                    U11 = UCL1 - u1
-                                    l1 = 1 / 3 * (X - LCL1)
-                                    l11 = 2 / 3 * (X - LCL1)
-                                    L1 = X - l11
-                                    L11 = X - l1
                                     pepe1 = pd.DataFrame({'x': mesh1})
                                     pepe2 = pd.DataFrame({'x': razmah})
                                     while n9 < L11:
-                                        n9 = rand.randint(int(L1), int(X))
+                                        n9 = rand.randint(int(L11), int(X))
                                         mesh1[23] = n9
                                         razmah.clear()
                                         razschet()
-                                        X = np.nanmean(pepe1.x)
-                                        R = np.nanmean(pepe2.x)
-                                        UCL1 = X + 2.660 * R
-                                        LCL1 = X - 2.660 * R
-                                        UCL2 = 3.267 * R
-                                        u1 = 1 / 3 * (UCL1 - X)
-                                        u11 = 2 / 3 * (UCL1 - X)
-                                        U1 = UCL1 - u11
-                                        U11 = UCL1 - u1
-                                        l1 = 1 / 3 * (X - LCL1)
-                                        l11 = 2 / 3 * (X - LCL1)
-                                        L1 = X - l11
-                                        L11 = X - l1
                                         pepe1 = pd.DataFrame({'x': mesh1})
                                         pepe2 = pd.DataFrame({'x': razmah})
 
