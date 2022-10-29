6ec839c5b7b3ea6e4efe5d2b190ab3a2d2fedf24
CypnkOV <major01_91@mail.ru>
2022-10-29 22:42:00 +0300

    0.5.5

--- Shuhart.py
+++ Shuhart.py
@@ -3,7 +3,7 @@ import numpy as np
 import random as rand
 import pandas as pd
 
-mesh1 = [rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100), rand.randint(0, 100)]
+mesh1 = [rand.randint(0, 80), rand.randint(0, 10), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80), rand.randint(0, 80)]
 pepe1 = pd.DataFrame({'x': mesh1})
 razmah = []
 
@@ -19,7 +19,7 @@ def razschet():
 razschet()
 pepe2 = pd.DataFrame({'x': razmah})
 
-n = 0
+n1 = n2 = n3 = n4 = n5 = n6 = n7 = n8 = n9 = -98989
 m = 0
 a1 = 0
 X = 0
@@ -86,6 +86,210 @@ while len(mesh1) != 15:
 
 # дописать третий критерий
 
+while len(mesh1) != 24 and n1 < L1 and n2 < L1 and n3 < L1 and n4 < L1 and n5 < L1 and n6 < L1 and n7 < L1 and n8 < L1 and n9 < L1:
+    X = np.nanmean(pepe1.x)
+    R = np.nanmean(pepe2.x)
+    UCL1 = X + 2.660 * R
+    LCL1 = X - 2.660 * R
+    UCL2 = 3.267 * R
+    u1 = 1 / 3 * (UCL1 - X)
+    u11 = 2 / 3 * (UCL1 - X)
+    U1 = UCL1 - u11
+    U11 = UCL1 - u1
+    l1 = 1 / 3 * (X - LCL1)
+    l11 = 2 / 3 * (X - LCL1)
+    L1 = X - l11
+    L11 = X - l1
+    if len(mesh1) != 24:
+        mesh1.append(0)
+        mesh1.append(0)
+        mesh1.append(0)
+        mesh1.append(0)
+        mesh1.append(0)
+        mesh1.append(0)
+        mesh1.append(0)
+        mesh1.append(0)
+        mesh1.append(0)
+    while n1 < L11:
+        n1 = rand.randint(int(L1), int(X))
+        mesh1[15] = n1
+        razmah.clear()
+        razschet()
+        X = np.nanmean(pepe1.x)
+        R = np.nanmean(pepe2.x)
+        UCL1 = X + 2.660 * R
+        LCL1 = X - 2.660 * R
+        UCL2 = 3.267 * R
+        u1 = 1 / 3 * (UCL1 - X)
+        u11 = 2 / 3 * (UCL1 - X)
+        U1 = UCL1 - u11
+        U11 = UCL1 - u1
+        l1 = 1 / 3 * (X - LCL1)
+        l11 = 2 / 3 * (X - LCL1)
+        L1 = X - l11
+        L11 = X - l1
+        pepe1 = pd.DataFrame({'x': mesh1})
+        pepe2 = pd.DataFrame({'x': razmah})
+        while n2 < L11:
+            n2 = rand.randint(int(L1), int(X))
+            mesh1[16] = n2
+            razmah.clear()
+            razschet()
+            X = np.nanmean(pepe1.x)
+            R = np.nanmean(pepe2.x)
+            UCL1 = X + 2.660 * R
+            LCL1 = X - 2.660 * R
+            UCL2 = 3.267 * R
+            u1 = 1 / 3 * (UCL1 - X)
+            u11 = 2 / 3 * (UCL1 - X)
+            U1 = UCL1 - u11
+            U11 = UCL1 - u1
+            l1 = 1 / 3 * (X - LCL1)
+            l11 = 2 / 3 * (X - LCL1)
+            L1 = X - l11
+            L11 = X - l1
+            pepe1 = pd.DataFrame({'x': mesh1})
+            pepe2 = pd.DataFrame({'x': razmah})
+            while n3 < L11:
+                n3 = rand.randint(int(L1), int(X))
+                mesh1[17] = n3
+                razmah.clear()
+                razschet()
+                X = np.nanmean(pepe1.x)
+                R = np.nanmean(pepe2.x)
+                UCL1 = X + 2.660 * R
+                LCL1 = X - 2.660 * R
+                UCL2 = 3.267 * R
+                u1 = 1 / 3 * (UCL1 - X)
+                u11 = 2 / 3 * (UCL1 - X)
+                U1 = UCL1 - u11
+                U11 = UCL1 - u1
+                l1 = 1 / 3 * (X - LCL1)
+                l11 = 2 / 3 * (X - LCL1)
+                L1 = X - l11
+                L11 = X - l1
+                pepe1 = pd.DataFrame({'x': mesh1})
+                pepe2 = pd.DataFrame({'x': razmah})
+                while n4 < L11:
+                    n4 = rand.randint(int(L1), int(X))
+                    mesh1[18] = n4
+                    razmah.clear()
+                    razschet()
+                    X = np.nanmean(pepe1.x)
+                    R = np.nanmean(pepe2.x)
+                    UCL1 = X + 2.660 * R
+                    LCL1 = X - 2.660 * R
+                    UCL2 = 3.267 * R
+                    u1 = 1 / 3 * (UCL1 - X)
+                    u11 = 2 / 3 * (UCL1 - X)
+                    U1 = UCL1 - u11
+                    U11 = UCL1 - u1
+                    l1 = 1 / 3 * (X - LCL1)
+                    l11 = 2 / 3 * (X - LCL1)
+                    L1 = X - l11
+                    L11 = X - l1
+                    pepe1 = pd.DataFrame({'x': mesh1})
+                    pepe2 = pd.DataFrame({'x': razmah})
+                    while n5 < L11:
+                        n5 = rand.randint(int(L1), int(X))
+                        mesh1[19] = n5
+                        razmah.clear()
+                        razschet()
+                        X = np.nanmean(pepe1.x)
+                        R = np.nanmean(pepe2.x)
+                        UCL1 = X + 2.660 * R
+                        LCL1 = X - 2.660 * R
+                        UCL2 = 3.267 * R
+                        u1 = 1 / 3 * (UCL1 - X)
+                        u11 = 2 / 3 * (UCL1 - X)
+                        U1 = UCL1 - u11
+                        U11 = UCL1 - u1
+                        l1 = 1 / 3 * (X - LCL1)
+                        l11 = 2 / 3 * (X - LCL1)
+                        L1 = X - l11
+                        L11 = X - l1
+                        pepe1 = pd.DataFrame({'x': mesh1})
+                        pepe2 = pd.DataFrame({'x': razmah})
+                        while n6 < L11:
+                            n6 = rand.randint(int(L1), int(X))
+                            mesh1[20] = n6
+                            razmah.clear()
+                            razschet()
+                            X = np.nanmean(pepe1.x)
+                            R = np.nanmean(pepe2.x)
+                            UCL1 = X + 2.660 * R
+                            LCL1 = X - 2.660 * R
+                            UCL2 = 3.267 * R
+                            u1 = 1 / 3 * (UCL1 - X)
+                            u11 = 2 / 3 * (UCL1 - X)
+                            U1 = UCL1 - u11
+                            U11 = UCL1 - u1
+                            l1 = 1 / 3 * (X - LCL1)
+                            l11 = 2 / 3 * (X - LCL1)
+                            L1 = X - l11
+                            L11 = X - l1
+                            pepe1 = pd.DataFrame({'x': mesh1})
+                            pepe2 = pd.DataFrame({'x': razmah})
+                            while n7 < L11:
+                                n7 = rand.randint(int(L1), int(X))
+                                mesh1[21] = n7
+                                razmah.clear()
+                                razschet()
+                                X = np.nanmean(pepe1.x)
+                                R = np.nanmean(pepe2.x)
+                                UCL1 = X + 2.660 * R
+                                LCL1 = X - 2.660 * R
+                                UCL2 = 3.267 * R
+                                u1 = 1 / 3 * (UCL1 - X)
+                                u11 = 2 / 3 * (UCL1 - X)
+                                U1 = UCL1 - u11
+                                U11 = UCL1 - u1
+                                l1 = 1 / 3 * (X - LCL1)
+                                l11 = 2 / 3 * (X - LCL1)
+                                L1 = X - l11
+                                L11 = X - l1
+                                pepe1 = pd.DataFrame({'x': mesh1})
+                                pepe2 = pd.DataFrame({'x': razmah})
+                                while n8 < L11:
+                                    n8 = rand.randint(int(L1), int(X))
+                                    mesh1[22] = n8
+                                    razmah.clear()
+                                    razschet()
+                                    X = np.nanmean(pepe1.x)
+                                    R = np.nanmean(pepe2.x)
+                                    UCL1 = X + 2.660 * R
+                                    LCL1 = X - 2.660 * R
+                                    UCL2 = 3.267 * R
+                                    u1 = 1 / 3 * (UCL1 - X)
+                                    u11 = 2 / 3 * (UCL1 - X)
+                                    U1 = UCL1 - u11
+                                    U11 = UCL1 - u1
+                                    l1 = 1 / 3 * (X - LCL1)
+                                    l11 = 2 / 3 * (X - LCL1)
+                                    L1 = X - l11
+                                    L11 = X - l1
+                                    pepe1 = pd.DataFrame({'x': mesh1})
+                                    pepe2 = pd.DataFrame({'x': razmah})
+                                    while n9 < L11:
+                                        n9 = rand.randint(int(L1), int(X))
+                                        mesh1[23] = n9
+                                        razmah.clear()
+                                        razschet()
+                                        X = np.nanmean(pepe1.x)
+                                        R = np.nanmean(pepe2.x)
+                                        UCL1 = X + 2.660 * R
+                                        LCL1 = X - 2.660 * R
+                                        UCL2 = 3.267 * R
+                                        u1 = 1 / 3 * (UCL1 - X)
+                                        u11 = 2 / 3 * (UCL1 - X)
+                                        U1 = UCL1 - u11
+                                        U11 = UCL1 - u1
+                                        l1 = 1 / 3 * (X - LCL1)
+                                        l11 = 2 / 3 * (X - LCL1)
+                                        L1 = X - l11
+                                        L11 = X - l1
+                                        pepe1 = pd.DataFrame({'x': mesh1})
+                                        pepe2 = pd.DataFrame({'x': razmah})
 
 print('индивидуальные значения ->', mesh1)
 print('размахи ->', razmah)
@@ -108,9 +312,9 @@ plt.axhline(y = X, color = 'red', linestyle = '-.', label = 'среднее', li
 plt.axhline(y = UCL1, color = 'red', linestyle = '--', label = 'верхняя', linewidth = '1')
 plt.axhline(y = LCL1, color = 'red', linestyle = '--', label = 'нижняя', linewidth = '1')
 plt.axhline(y = U1, color = 'red', linestyle = '--', linewidth = '1')
-plt.axhline(y = U11, color = 'red', linestyle = '--', linewidth = '1')
+plt.axhline(y = U11, color = 'red', linestyle = '--', linewidth = '1') # 2 по счету линия
 plt.axhline(y = L1, color = 'red', linestyle = '--', linewidth = '1')
-plt.axhline(y = L11, color = 'red', linestyle = '--', linewidth = '1')
+plt.axhline(y = L11, color = 'red', linestyle = '--', linewidth = '1') # 5 по счету линия
 
 # отображение графика индивид.
 
